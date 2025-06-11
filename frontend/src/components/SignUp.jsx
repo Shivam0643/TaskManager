705ffefe0c09ad8d700a85e961fd5da9616de3b3
Shivam Lashkari <159267600+Shivam0643@users.noreply.github.com>
2025-06-11 20:54:35 +0530

    Update SignUp.jsx

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { BUTTONCLASSES, FIELDS, Inputwrapper } from "../assets/dummy";
 import axios from "axios";
 
-const API_URL = "http://localhost:4000";
+const API_URL = "https://taskmanager-backend-n7qg.onrender.com";
 const INITIAL_FORM = { name: "", email: "", password: "" };
 
 const SignUp = ({ onSwitchMode }) => {
